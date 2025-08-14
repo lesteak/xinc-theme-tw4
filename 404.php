746b4adef5bbe7365666a739d249dc564ca49027
Beef <mark@benewith.com>
2025-08-14 20:43:47 +0100

    add 404 template

--- 404.php
+++ 404.php
@@ -1,9 +1,11 @@
 <?php get_header(); ?>
 
 <div class="container">
-  <main>
-    <h1 class="text-xl">404</h1>
-    <?php _e('Sorry, but the page you were trying to view does not exist.', 'tofino'); ?>
+  <main class="space-y-8">
+    <h1 class="text-2xl font-bold">404</h1>
+    <div>
+      <?php _e('Sorry, but the page you were trying to view does not exist.', 'xinc-theme'); ?>
+    </div>
   </main>
 </div>
 
